8d2fe95c186c969014c101d7aac9c08fcfbade70
Nahuel <nahugonzalez11@gmail.com>
2018-06-19 10:38:19 -0300

    cambios en el juego de sistemas materiales

--- TrabajoPracticoTED/TrabajoPracticoTED/SistemaMaterialesForm.Designer.cs
+++ TrabajoPracticoTED/TrabajoPracticoTED/SistemaMaterialesForm.Designer.cs
@@ -28,19 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_Final = new System.Windows.Forms.Button();
+            this.txt_Puntaje = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(229, 259);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(75, 23);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "Pregunta 1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(197, 230);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(133, 23);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.Text = "Pregunta 2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(170, 201);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(184, 23);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.Text = "Pregunta 3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            // 
+            // btn_4
+            // 
+            this.btn_4.Location = new System.Drawing.Point(150, 172);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(225, 23);
+            this.btn_4.TabIndex = 3;
+            this.btn_4.Text = "Pregunta 4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            // 
+            // btn_5
+            // 
+            this.btn_5.Location = new System.Drawing.Point(136, 143);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(255, 23);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.Text = "Pregunta 5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            // 
+            // btn_6
+            // 
+            this.btn_6.Location = new System.Drawing.Point(126, 114);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(281, 23);
+            this.btn_6.TabIndex = 5;
+            this.btn_6.Text = "Pregunta 6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            // 
+            // btn_7
+            // 
+            this.btn_7.Location = new System.Drawing.Point(113, 85);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(312, 23);
+            this.btn_7.TabIndex = 6;
+            this.btn_7.Text = "Pregunta 7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            // 
+            // btn_Final
+            // 
+            this.btn_Final.Location = new System.Drawing.Point(97, 56);
+            this.btn_Final.Name = "btn_Final";
+            this.btn_Final.Size = new System.Drawing.Size(343, 23);
+            this.btn_Final.TabIndex = 7;
+            this.btn_Final.Text = "Pregunta Final";
+            this.btn_Final.UseVisualStyleBackColor = true;
+            // 
+            // txt_Puntaje
+            // 
+            this.txt_Puntaje.Location = new System.Drawing.Point(503, 59);
+            this.txt_Puntaje.Name = "txt_Puntaje";
+            this.txt_Puntaje.Size = new System.Drawing.Size(100, 20);
+            this.txt_Puntaje.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(503, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Puntaje";
             // 
             // SistemaMaterialesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(758, 324);
+            this.ClientSize = new System.Drawing.Size(641, 317);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Puntaje);
+            this.Controls.Add(this.btn_Final);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
             this.Name = "SistemaMaterialesForm";
             this.Text = "SistemaMaterialesForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_Final;
+        private System.Windows.Forms.TextBox txt_Puntaje;
+        private System.Windows.Forms.Label label1;
     }
 }
